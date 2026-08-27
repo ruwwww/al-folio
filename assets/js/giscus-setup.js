2bb17af290a9052db22491f847d20447bf0d2be5
ruwwww <ruwwww@users.noreply.github.com>
2026-08-27 15:17:18 +0000

    Deploying to gh-pages from @ ruwwww/ruwwww.github.io@365e3f8c4d69305a902094982b5b70ba8ae4ebcd 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "ruwwww/al-folio",
+    "data-repo": "ruwwww/ruwwww.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
